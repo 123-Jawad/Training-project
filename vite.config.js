37529c2dbd5e2ede9404a1efb63afd86bfc0a7d7
j <j@gmail.com>
2026-02-25 01:51:43 +0500

    new change

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue2 from '@vitejs/plugin-vue2'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Training-project/',
+  base: '/Whistleit-project/',
   plugins: [
     vue2(),
     legacy({
